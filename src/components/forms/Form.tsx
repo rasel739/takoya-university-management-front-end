12a19206c50b4e0da6137a82fab5f5275a91dec3
rasel739 <raselhossaintro@gmail.com>
2025-07-25 07:25:52 +0600

    form provider code update

--- src/components/forms/Form.tsx
+++ src/components/forms/Form.tsx
@@ -1,29 +1,51 @@
-import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+// import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
-type FormConfig = {
-  defaultValue?: Record<string, any>;
+import { FieldValues, FormProvider, SubmitHandler, useForm, UseFormProps } from 'react-hook-form';
+
+// type FormConfig = {
+//   defaultValue?: Record<string, any>;
+// };
+
+// type FormProps = {
+//   children: React.ReactNode | React.ReactElement;
+//   submitHandler: SubmitHandler<any>;
+// } & FormConfig;
+
+// const Form = ({ children, submitHandler, defaultValue }: FormProps) => {
+//   const formConfig: FormConfig = {};
+//   const methods = useForm<FormProps>(formConfig);
+
+//   const { handleSubmit, reset } = methods;
+//   const onSubmit = (data: any) => {
+//     submitHandler(data);
+//     reset(); // Reset the form after submission
+//   };
+
+//   if (!!defaultValue) formConfig['defaultValue'] = defaultValue;
+
+//   return (
+//     <FormProvider {...methods}>
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         {children}
+//         <input type='submit' value='Submit' />
+//       </form>
+//     </FormProvider>
+//   );
+// };
+
+// export default Form;
+
+type FormProps<T extends FieldValues> = {
+  children: React.ReactNode | React.ReactElement;
+  onSubmit: SubmitHandler<T>;
+  options?: UseFormProps<T>;
 };
 
-type FormProps = {
-  children: React.ReactNode | React.ReactElement;
-  submitHandler: SubmitHandler<any>;
-} & FormConfig;
-
-const Form = ({ children, submitHandler, defaultValue }: FormProps) => {
-  const formConfig: FormConfig = {};
-  const methods = useForm<FormProps>(formConfig);
-
-  const { handleSubmit, reset } = methods;
-  const onSubmit = (data: any) => {
-    submitHandler(data);
-    reset(); // Reset the form after submission
-  };
-
-  if (!!defaultValue) formConfig['defaultValue'] = defaultValue;
-
+const Form = <T extends FieldValues>({ children, onSubmit, options }: FormProps<T>) => {
+  const methods = useForm<T>(options);
   return (
     <FormProvider {...methods}>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} noValidate>
         {children}
         <input type='submit' value='Submit' />
       </form>

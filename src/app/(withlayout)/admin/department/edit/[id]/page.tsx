7f8214e384c8c0929cca8aec29371e609a52775c
rasel739 <raselhossaintro@gmail.com>
2025-10-01 19:34:35 +0600

    fix edit department

--- src/app/(withlayout)/admin/department/edit/[id]/page.tsx
+++ src/app/(withlayout)/admin/department/edit/[id]/page.tsx
@@ -1,7 +1,9 @@
 import DepartmentEdit from '@/components/ui/department-edit';
 
-const EditDepartmentPage = async ({ params }: { params: { id: string } }) => {
-  return <DepartmentEdit id={params.id} />;
+const EditDepartmentPage = async ({ params }: { params: { [key: string]: string | string[] } }) => {
+  const id = Array.isArray(params.id) ? params.id[0] : params.id;
+
+  return <DepartmentEdit id={id} />;
 };
 
 export default EditDepartmentPage;
